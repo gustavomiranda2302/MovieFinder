a09ac90a8cadb34e01e86cbf43d8aa22c45efa23
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-06-09 17:18:11 -0300

    feat: fetchMovie e omdbMapper

--- src/service/omdb.ts
+++ src/service/omdb.ts
@@ -16,3 +16,14 @@ export function omdbMapper(Response: OmdbResponse): Movie {
     poster: Response.Poster,
   };
 }
+export async function fetchMovie(search: string): Promise<Movie> {
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const url: string = `https://www.omdbapi.com/?apikey=${apiKey}&s=${search}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`erro http status:${response.status}`);
+  }
+  const data = await response.json();
+  return omdbMapper(data);
+}
