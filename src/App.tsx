f8ef0a996faf4626361d7a1fd369cb50c48edc0c
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-06-10 16:42:17 -0300

    funcao handleSearch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-
-
+import { useState } from "react";
+import type { Movie } from "./types/movie"
+import { SearchBar } from "./components/SearchBar"
+import { MovieList } from "./components/MovieList"
+import { fetchMovie } from "./service/omdb";
 
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  async function handleSearch(s: string) {
+    setLoading(true);
+    try {
+      const result = await fetchMovie(s);
+      setMovies([result]);
+    } catch (error) {
+      setError("filme nao encontrado")
+
+    } finally {
+      setLoading(false)
+    }
+
+
+
+  }
 
   return (
     <div></div>
